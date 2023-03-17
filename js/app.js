56029f2f5b20226a10bab735d12f79833fffd030
Mohammad Jubair <you@example.com>
2023-03-18 00:17:11 +0600

    final push

--- js/app.js
+++ js/app.js
@@ -1,5 +1,23 @@
-let number = 0;
+let images = [
+  "img/img1.jpg",
+  "img/img2.jpg",
+  "img/img3.jpg",
+  "img/img4.jpg",
+  "img/img5.jpg",
+  "img/img6.jpg",
+  "img/img7.jpg",
+  "img/img8.jpg",
+  "img/img9.jpg",
+];
 
+let imgId = document.getElementById("img-link");
+let imgIndex = 0;
 setInterval(() => {
-  number++;
-}, 1000);
+  if (imgIndex === images.length) {
+    imgIndex = 0;
+  }
+
+  let imglink = images[imgIndex];
+  imgId.setAttribute("src", imglink);
+  imgIndex++;
+}, 100);
